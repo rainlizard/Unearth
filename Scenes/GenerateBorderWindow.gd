34ceac48327527ab5d28c55154f6132e4e68cc98
rainlizard <ludorverr@gmail.com>
2022-12-03 21:36:26 +1100

    .

--- Scenes/GenerateBorderWindow.gd
+++ Scenes/GenerateBorderWindow.gd
@@ -25,9 +25,27 @@ func _ready():
 	reinit_noise_preview()
 
 func reinit_noise_preview():
-	imageData.create(oXSizeLine.text.to_int(), oYSizeLine.text.to_int(), false, Image.FORMAT_RGB8)
+	var sizeX = oXSizeLine.text.to_int()
+	var sizeY = oYSizeLine.text.to_int()
+	imageData.create(sizeX, sizeY, false, Image.FORMAT_RGB8)
 	textureData.create_from_image(imageData, 0)
 	oQuickNoisePreview.texture = textureData
+	
+	
+	var pixelSize = 4
+	var maxSize = Vector2(85*pixelSize,85*pixelSize)
+	
+	oQuickNoisePreview.rect_min_size = Vector2(min(sizeX*pixelSize, maxSize.x), min(sizeY*pixelSize, maxSize.y))
+	
+	if sizeX > 85 or sizeY > 85:
+		if sizeX < sizeY:
+			var aspectRatio = float(max(1.0,sizeX)) / float(max(1.0,sizeY))
+			oQuickNoisePreview.rect_min_size.x *= aspectRatio
+		else:
+			var aspectRatio = float(max(1.0,sizeY)) / float(max(1.0,sizeX))
+			oQuickNoisePreview.rect_min_size.y *= aspectRatio
+	
+	oQuickNoisePreview.rect_size = oQuickNoisePreview.rect_min_size
 
 func _on_NewMapWindow_visibility_changed():
 	if visible == true:
@@ -94,11 +112,30 @@ func update_border_image_with_noise():
 	
 	var floodFillTileMap = TileMap.new()
 	
+	var aspectRatio = Vector2()
+	if fullMapSize.x < fullMapSize.y:
+		aspectRatio.x = max(fullMapSize.x,1.0) / max(fullMapSize.y,1.0)
+		aspectRatio.y = 1.0
+	else:
+		aspectRatio.x = 1.0
+		aspectRatio.y = max(fullMapSize.y,1.0) / max(fullMapSize.x,1.0)
+	
+	var edgeDist = Vector2()
 	for x in range(1, fullMapSize.x):
 		for y in range(1, fullMapSize.y):
-			var edgeDistPercent = 1.0 - (max(abs(x-halfMapSize.x), abs(y-halfMapSize.y)) / min(halfMapSize.x,halfMapSize.y))
-			if abs(noise.get_noise_2d(x/fullMapSize.x, y/fullMapSize.y)) < edgeDistPercent:
+			edgeDist.x = 1.0 - (abs(x-halfMapSize.x) / halfMapSize.x)
+			edgeDist.y = 1.0 - (abs(y-halfMapSize.y) / halfMapSize.y)
+			
+			var n = abs(noise.get_noise_2d( (x/fullMapSize.x)*aspectRatio.x, (y/fullMapSize.y)*aspectRatio.y ))
+			
+			if n < edgeDist.x and n < edgeDist.y:
 				floodFillTileMap.set_cell(x,y,1)
+	
+#	for x in range(1, fullMapSize.x):
+#		for y in range(1, fullMapSize.y):
+#			var edgeDistPercent = 1.0 - (max(abs(x-halfMapSize.x), abs(y-halfMapSize.y)) / max(halfMapSize.x,halfMapSize.y))
+#			if abs(noise.get_noise_2d(x/fullMapSize.x, y/fullMapSize.y)) < edgeDistPercent:
+#				floodFillTileMap.set_cell(x,y,1)
 	
 	var coordsToCheck = [Vector2(halfMapSize.x,halfMapSize.y)]
 	
