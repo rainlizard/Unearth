cf32df5c691fb8d8eedf5720dc1a6dcddd032a11
rainlizard <ludorverr@gmail.com>
2023-02-12 11:25:29 +1100

    0.32

--- Scenes/TextureEditingWindow.gd
+++ Scenes/TextureEditingWindow.gd
@@ -11,11 +11,17 @@ onready var oRNC = Nodelist.list["oRNC"]
 onready var oGame = Nodelist.list["oGame"]
 onready var oMessage = Nodelist.list["oMessage"]
 onready var oExportTmapaButton = Nodelist.list["oExportTmapaButton"]
+onready var oMapProperties = Nodelist.list["oMapProperties"]
+onready var oDataLevelStyle = Nodelist.list["oDataLevelStyle"]
+onready var oEditor = Nodelist.list["oEditor"]
 
 
 var filelistfile = File.new()
 var fileListFilePath = ""
 var editingImg = Image.new()
+var fileTimes = []
+var partsList = []
+var modifiedCheck = File.new()
 
 
 func _ready():
@@ -24,6 +30,7 @@ func _ready():
 	oExportTmapaButton.disabled = true
 	oExportTmapaButton.set_tooltip("A filelist pack must be loaded first in order to export")
 
+
 func _on_ChooseFileListFileDialog_file_selected(path):
 	fileListFilePath = path
 	oReloaderPathLabel.text = path
@@ -31,23 +38,119 @@ func _on_ChooseFileListFileDialog_file_selected(path):
 	oReloaderContainer.visible = true
 	oExportTmapaButton.disabled = false
 	oExportTmapaButton.set_tooltip("")
+	
+	initialize_filelist()
+	
 	reloader_loop()
 
+
 func reloader_loop():
-	var timerNumber = float(oReloadEveryLineEdit.text)
-	print(timerNumber)
-	yield(get_tree().create_timer(timerNumber), "timeout")
-	
 	if fileListFilePath != "":
 		execute()
 	
+	var timerNumber = float(oReloadEveryLineEdit.text)
+	#print(timerNumber)
+	yield(get_tree().create_timer(timerNumber), "timeout")
+	
 	reloader_loop()
+
+
+func initialize_filelist():
+	partsList.clear()
+	
+	if filelistfile.open(fileListFilePath, File.READ) != OK: return
+	
+	var flContent = filelistfile.get_as_text()
+	filelistfile.close()
+	
+	partsList = Array(flContent.split('\n', false))
+	partsList.pop_front() # remove the first line: textures_pack_000	8	68	32	32
+	
+	for i in partsList.size():
+		partsList[i] = Array(partsList[i].split('\t', false))
+		#print(partsList[i].size())
+	
+	# Initialize fileTimes
+	if fileTimes.size() < partsList.size():
+		fileTimes.resize(partsList.size()) # This is bigger than it needs to be but that doesn't matter
+		for i in fileTimes.size():
+			fileTimes[i] = -1
+	
+	# Change current map's Dungeon Style setting if it's a different one
+	var fn = get_tmapa_filename()
+	if oDataLevelStyle.data != int(fn):
+		oDataLevelStyle.data = int(fn)
+		oTextureCache.set_current_texture_pack()
+		oEditor.mapHasBeenEdited = true
+		oMessage.quick("Changed map's Dungeon style to show what you're currently editing")
+
+
+func execute():
+	var CODETIME_START = OS.get_ticks_msec()
+	
+	var anyChangesWereMade = false
+	var imgLoader = Image.new()
+	var baseDir = fileListFilePath.get_base_dir()
+	var partsModified = []
+	
+	
+	for i in partsList.size():
+		var path = baseDir.plus_file(partsList[i][0])
+		if modifiedCheck.get_modified_time(path) != fileTimes[i]:
+			partsModified.append(i)
+	
+	
+	for i in partsModified:
+		var path = baseDir.plus_file(partsList[i][0])
+		fileTimes[i] = modifiedCheck.get_modified_time(path) # Must be in a separate loop to the if check for modified time
+		
+		#print("A FILE WAS CHANGED")
+		
+		var x = int(partsList[i][1])
+		var y = int(partsList[i][2])
+		var w = int(partsList[i][3])
+		var h = int(partsList[i][4])
+		imgLoader.load(path)
+		imgLoader.convert(Image.FORMAT_RGB8)
+		
+		var destY = i/8
+		var destX = i-(destY*8)
+		
+		var destination = Vector2(destX*32,destY*32)
+		editingImg.lock()
+		imgLoader.lock()
+		for pixelY in imgLoader.get_height(): # This is different than using the w and h variables
+			for pixelX in imgLoader.get_width():
+				var col = imgLoader.get_pixel(pixelX,pixelY)
+				if oReadPalette.dictionary.has(col) == false:
+					imgLoader.set_pixel(pixelX,pixelY, Color(255,0,255))
+		
+		editingImg.blit_rect(imgLoader,Rect2(x,y,w,h), destination)
+		imgLoader.unlock()
+		editingImg.unlock()
+		anyChangesWereMade = true
+	
+	if anyChangesWereMade == true:
+		var tmapaNumber = get_tmapa_filename()
+		oTextureCache.load_image_into_cache(editingImg, tmapaNumber)
+		oTextureCache.set_current_texture_pack()
+	
+	print('Codetime: ' + str(OS.get_ticks_msec() - CODETIME_START) + 'ms')
+
+#	var imgTex = ImageTexture.new()
+#	imgTex.create_from_image(img,0)
+#	$"../TextureRect".texture = imgTex
+
+
 
 func _on_LoadFilelistButton_pressed():
 	Utils.popup_centered(oChooseFileListFileDialog)
 	oChooseFileListFileDialog.current_dir = Settings.unearth_path.plus_file("textures").plus_file("")
 	oChooseFileListFileDialog.current_path = Settings.unearth_path.plus_file("textures").plus_file("")
 	oChooseFileListFileDialog.current_file = "filelist_tmapa000.txt"
+	print(oChooseFileListFileDialog.get_vbox())
+
+
 
 func _on_ExportTmapaButton_pressed():
 	Utils.popup_centered(oExportTmapaDatDialog)
@@ -61,55 +164,6 @@ func _on_CreateFilelistButton_pressed():
 	oChooseTmapaFileDialog.current_path = oGame.DK_DATA_DIRECTORY.plus_file("")
 	oChooseTmapaFileDialog.current_file = "tmapa000.dat"
 
-func execute():
-	var baseDir = fileListFilePath.get_base_dir()
-	
-	if filelistfile.open(fileListFilePath, File.READ) != OK: return
-	
-	var flContent = filelistfile.get_as_text()
-	filelistfile.close()
-	
-	var lineArray = Array(flContent.split('\n', false))
-	lineArray.pop_front() # remove the first line: textures_pack_000	8	68	32	32
-	
-	for i in lineArray.size():
-		lineArray[i] = Array(lineArray[i].split('\t', false))
-		#print(lineArray[i].size())
-	
-	
-	#img.fill(Color(1,1,1,1))
-	editingImg.lock()
-	var imgLoader = Image.new()
-	var CODETIME_START = OS.get_ticks_msec()
-	for i in lineArray.size():
-		#if i == 40:
-			#print(lineArray[i])
-			var path = lineArray[i][0]
-			var x = lineArray[i][1]
-			var y = lineArray[i][2]
-			var width = lineArray[i][3]
-			var height = lineArray[i][4]
-			imgLoader.load(baseDir.plus_file(path))
-			
-			var destY = i/8
-			var destX = i-(destY*8)
-			
-			var destination = Vector2(destX*32,destY*32)
-			
-			editingImg.blit_rect(imgLoader,Rect2(x,y,width,height), destination)
-			#for z in lineArray[i]:
-			#	print(z)
-	editingImg.unlock()
-	print('Codetime: ' + str(OS.get_ticks_msec() - CODETIME_START) + 'ms')
-	
-	
-	
-	var tmapaNumber = get_tmapa_filename()
-	oTextureCache.load_image_into_cache(editingImg, tmapaNumber)
-	oTextureCache.set_current_texture_pack()
-#	var imgTex = ImageTexture.new()
-#	imgTex.create_from_image(img,0)
-#	$"../TextureRect".texture = imgTex
 
 func get_tmapa_filename():
 	return fileListFilePath.right(fileListFilePath.length()-12).to_lower().trim_suffix(".txt")
