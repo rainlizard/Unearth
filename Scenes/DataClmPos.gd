3ea576b6cc4f1f996d71a431bcd38d1d3afd9e9a
rainlizard <15337628+rainlizard@users.noreply.github.com>
2024-04-12 09:08:52 +1000

    -

--- Scenes/DataClmPos.gd
+++ Scenes/DataClmPos.gd
@@ -4,21 +4,18 @@ extends Grid
 func get_cell_clmpos(x, y):
 	if is_valid_coordinate(x, y) == false:
 		return 0
+	
 	buffer.seek((y*width+x) * 2)
-	var value = 65536 - buffer.get_u16()
-	if value == 65536: value = 0
-	return value
-
+	return abs(buffer.get_16())
 
 func get_cell_clmpos_fast(x, y):
 	buffer.seek((y*width+x) * 2)
-	var value = 65536 - buffer.get_u16()
-	if value == 65536: value = 0
-	return value
-
+	return abs(buffer.get_16())
 
 func set_cell_clmpos(x, y, value):
-	value = 65536-value
-	if value == 65536: value = 0
+	value = value
 	set_cell(x, y, value)
 
+# Alternatively, use:
+#var value = 65536 - buffer.get_u16()
+#if value == 65536: value = 0
