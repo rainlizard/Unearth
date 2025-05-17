a7eaa7ef517ca63e4500195802b46a8a0db57268
rainlizard <15337628+rainlizard@users.noreply.github.com>
2025-05-17 14:43:04 +1000

    Handle tabbing to next UI element (Properties window excluded)

--- Scenes/Ui.gd
+++ Scenes/Ui.gd
@@ -20,6 +20,8 @@ onready var o3DCameraInfo = Nodelist.list["o3DCameraInfo"]
 onready var oSelection = Nodelist.list["oSelection"]
 onready var oSelector = Nodelist.list["oSelector"]
 
+var tabKeyInputEvent = InputEventKey.new()
+
 var FONT_SIZE_CR_LVL_BASE := 1.00 setget set_FONT_SIZE_CR_LVL_BASE
 var FONT_SIZE_CR_LVL_MAX := 8.00 setget set_FONT_SIZE_CR_LVL_MAX
 
@@ -28,24 +30,29 @@ const topMargin = 69
 var optionButtonIsOpened = false
 var mouseOnUi = false
 var _is_handling_drag = false
-
 var listOfWindowDialogs = []
 
 func _ready():
+	tabKeyInputEvent.scancode = KEY_TAB
+	setup_focus_key()
+	find_window_dialogs()
+	wait_until_windows_are_positioned()
+	get_viewport().connect("gui_focus_changed", self, "_on_gui_focus_changed")
+
+func wait_until_windows_are_positioned():
+	for i in 10:
+		yield(get_tree(),'idle_frame')
+	for window in listOfWindowDialogs:
+		window.connect("item_rect_changed",self,"_on_any_window_was_dragged",[window])
+
+func setup_focus_key():
+	InputMap.action_add_event("ui_focus_next", tabKeyInputEvent)
+
+func find_window_dialogs():
 	for mainCategories in get_children():
 		for potentialWindow in mainCategories.get_children():
 			if potentialWindow is WindowDialog:
 				listOfWindowDialogs.append(potentialWindow)
-	
-	get_viewport().connect("size_changed", self, "_on_viewport_size_changed") # Connect viewport resize handler
-
-	for i in 10: # Important to wait here, so the viewport size/resolution doesn't affect window positions
-		yield(get_tree(),'idle_frame')
-	
-	_on_viewport_size_changed()
-	
-	for windowNode in listOfWindowDialogs:
-		windowNode.connect("item_rect_changed",self,"_on_any_window_was_dragged",[windowNode])
 
 func _clamp_window_position(theWindow, currentViewSize):
 	if theWindow.rect_position.x > currentViewSize.x - theWindow.rect_size.x:
@@ -76,14 +83,32 @@ func _on_viewport_size_changed():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouseOnUi = true # This line combined with the line in _unhandled_input can be used to determine whether the mouse is over UI.
+		mouseOnUi = true
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
+		process_unfocused_click()
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		mouseOnUi = false # Used in combination with the line in _input()
-		# There's a Godot bug where if you open an optionbutton, it treats it as if the mouse is not on UI.
+		mouseOnUi = false
 		if optionButtonIsOpened == true:
 			mouseOnUi = true
 
+func process_unfocused_click():
+	if is_instance_valid(oPropertiesWindow) == false:
+		return
+	var focusedControl = oPropertiesWindow.get_focus_owner()
+	if focusedControl != null:
+		if is_instance_valid(focusedControl):
+			call_deferred("check_release_focus", focusedControl)
+
+func check_release_focus(controlToCheck):
+	if is_instance_valid(controlToCheck) == false:
+		return
+	if is_instance_valid(oPropertiesWindow) == false:
+		return
+	var currentFocusOwner = oPropertiesWindow.get_focus_owner()
+	if currentFocusOwner == controlToCheck:
+		controlToCheck.release_focus()
 
 func update_theme_colour(val):
 	var col = Constants.windowTitleCol[val]
@@ -93,8 +118,6 @@ func update_theme_colour(val):
 func HSV_8(h,s,v):
 	return Color.from_hsv(h/359.0,s/100.0,v/100.0, 1.0)
 
-#oUi2D.theme.set('WindowDialog/colors/title_color', col)
-
 func set_FONT_SIZE_CR_LVL_BASE(setVal):
 	FONT_SIZE_CR_LVL_BASE = setVal
 	oCamera2D.emit_signal("zoom_level_changed", oCamera2D.zoom)
@@ -102,22 +125,6 @@ func set_FONT_SIZE_CR_LVL_BASE(setVal):
 func set_FONT_SIZE_CR_LVL_MAX(setVal):
 	FONT_SIZE_CR_LVL_MAX = setVal
 	oCamera2D.emit_signal("zoom_level_changed", oCamera2D.zoom)
-
-
-
-
-#func opened_2D_view():
-#	if is_instance_valid(oPickThingWindow) == false: return
-#	if oImageAsMapDialog.visible == true: return
-#	oUi3D.visible = false
-#	show_tools()
-
-#func opened_3D_view():
-#	if is_instance_valid(oPickThingWindow) == false: return
-#	oUi3D.visible = true
-	
-	
-
 
 func show_tools():
 	if oDataSlab.get_cell(0,0) == TileMap.INVALID_CELL:
@@ -146,8 +153,6 @@ func hide_tools():
 func switch_to_2D():
 	o3DCameraInfo.visible = false
 	if oDataSlab.get_cell(0,0) != TileMap.INVALID_CELL:
-		#oUi3D.visible = false
-		# Don't show tools if opening a map from the map browser or oImageAsMapDialog
 		if oMapBrowser.visible == false and oImageAsMapDialog.visible == false:
 			show_tools()
 	else:
@@ -162,16 +167,25 @@ func switch_to_1st_person():
 	o3DCameraInfo.visible = o3DCameraInfo.ENABLE_CAMERA_COORDS
 	oPlayer.switch_camera_type(1)
 	hide_tools()
-	
-	# This code section below is temporary.
-#	yield(get_tree(),'idle_frame')
-#	if oGenerateTerrain.GENERATED_TYPE == oGenerateTerrain.GEN_CLM:
-#		oPropertiesWindow.visible = true
-#		oUi3D.visible = true
-#
-#		oPropertiesWindow.oPropertiesTabs.current_tab = 2
-
 
 func set_ui_scale(setVal):
 	Settings.UI_SCALE = Vector2(setVal,setVal)
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_IGNORE, Vector2(1024,576), setVal)
+
+func _on_gui_focus_changed(newlyFocusedControl):
+	if is_instance_valid(oPropertiesWindow) == false:
+		if InputMap.action_has_event("ui_focus_next", tabKeyInputEvent) == false:
+			InputMap.action_add_event("ui_focus_next", tabKeyInputEvent)
+		return
+
+	var focusIsInsideProperties = false
+	if newlyFocusedControl != null:
+		if newlyFocusedControl == oPropertiesWindow or oPropertiesWindow.is_a_parent_of(newlyFocusedControl):
+			focusIsInsideProperties = true
+	
+	if focusIsInsideProperties == true:
+		if InputMap.action_has_event("ui_focus_next", tabKeyInputEvent):
+			InputMap.action_erase_event("ui_focus_next", tabKeyInputEvent)
+	else:
+		if InputMap.action_has_event("ui_focus_next", tabKeyInputEvent) == false:
+			InputMap.action_add_event("ui_focus_next", tabKeyInputEvent)
