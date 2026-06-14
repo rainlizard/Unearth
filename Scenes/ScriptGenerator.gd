365d2a173c2789867a746cbb1fd531bfebd65c92
rainlizard <15337628+rainlizard@users.noreply.github.com>
2026-06-14 14:31:06 +1000

    Script Generator improvements

--- Scenes/ScriptGenerator.gd
+++ Scenes/ScriptGenerator.gd
@@ -34,6 +34,7 @@ onready var oOrangeAICheckBox = Nodelist.list["oOrangeAICheckBox"]
 onready var oCurrentFormat = Nodelist.list["oCurrentFormat"]
 onready var oScriptEditorWindow = Nodelist.list["oScriptEditorWindow"]
 onready var oScriptGeneratorWindow = Nodelist.list["oScriptGeneratorWindow"]
+onready var oConfigFileManager = Nodelist.list["oConfigFileManager"]
 
 var scnAvailableButton = preload('res://Scenes/AvailableButton.tscn')
 
@@ -93,20 +94,9 @@ var listMagic = [
 [Things.SPELLBOOK.DESTROY_WALLS, "POWER_DESTROY_WALLS", 0],
 ]
 
-var listTrap = [
-[2, "ALARM", 1],
-[3, "POISON_GAS", 1],
-[4, "LIGHTNING", 1],
-[6, "LAVA", 1],
-[1, "BOULDER", 1],
-[5, "WORD_OF_POWER", 1],
-]
-var listDoor = [
-[1, "WOOD", 1],
-[2, "BRACED", 1],
-[3, "STEEL", 1],
-[4, "MAGIC", 1],
-]
+const CLASSIC_TRAP_ORDER = [2, 3, 4, 6, 1, 5]
+const CLASSIC_DOOR_ORDER = [1, 2, 3, 4]
+const HIDDEN_CREATURES = [23, 31]
 var listRoom = [
 [Slabs.TREASURE_ROOM, "TREASURE", 1],
 [Slabs.LAIR, "LAIR", 1],
@@ -186,6 +176,10 @@ func update_options_based_on_mapformat():
 		oPurpleAICheckBox.visible = true
 		oBlackAICheckBox.visible = true
 		oOrangeAICheckBox.visible = true
+	initialize_rooms_available()
+	initialize_creatures_available()
+	initialize_traps_available()
+	initialize_doors_available()
 
 
 func initialize_researchables():
@@ -227,29 +221,33 @@ func initialize_researchables():
 		oResearchables.add_child(idItem)
 
 func initialize_rooms_available():
-	for i in listRoom:
-		var slabID = i[0]# listRoom[] #Slabs.roomMenuOrder[i]
+	var currentStates = get_existing_states(oRoomsAvailable)
+	clear_available_buttons(oRoomsAvailable)
+	for i in get_room_list():
+		var slabID = i[0]
 		var functionVariable = i[1]
 		var defaultAvailability = i[2]
-		
+		var getName = Slabs.fetch_name(slabID)
 		var slabName = Slabs.fetch_idname(slabID)
-		if Slabs.icons.has(slabName):
-			var id = scnAvailableButton.instance()
-			id.hint_tooltip = Slabs.data[slabID][Slabs.NAME] + ' availability'
-			id.get_node("IconTextureRect").texture = Slabs.icons.get(slabName, null)
-			id.set_meta("variable", functionVariable)
-			id.set_meta("ID", slabID)
-			id.get_node("%TextEditableLabel").editable = false
-			
-			if defaultAvailability == 1:
-				id.set_availability_state(id.OPTION_START)
-			else:
-				id.set_availability_state(id.OPTION_RESEARCH)
-			
-			oRoomsAvailable.add_child(id)
+		var id = scnAvailableButton.instance()
+		id.hint_tooltip = getName + ' availability'
+		set_button_texture(id, Slabs.icons.get(slabName, null), getName)
+		id.set_meta("variable", functionVariable)
+		id.set_meta("ID", slabID)
+		id.get_node("%TextEditableLabel").editable = false
+		if currentStates.has(functionVariable):
+			id.set_availability_state(currentStates[functionVariable])
+		elif defaultAvailability == 1:
+			id.set_availability_state(id.OPTION_START)
+		else:
+			id.set_availability_state(id.OPTION_RESEARCH)
+		oRoomsAvailable.add_child(id)
 
 func initialize_creatures_available(): # oCreaturePool
-	for i in listCreature:
+	var currentStates = get_existing_creature_data()
+	clear_available_buttons(oCreaturePool)
+	clear_available_buttons(oHeroPool)
+	for i in get_creature_list():
 		var subtype = i[0]
 		var functionVariable = i[1]
 		var defaultAvailability = i[2]
@@ -257,41 +255,29 @@ func initialize_creatures_available(): # oCreaturePool
 		id.set_meta("variable", functionVariable)
 		var getName = Things.fetch_name(Things.TYPE.CREATURE, subtype)
 		id.hint_tooltip = getName + ' availability'
-		id.get_node("%IconTextureRect").texture = Things.fetch_sprite(Things.TYPE.CREATURE, subtype)
+		set_creature_texture(id, subtype, i[4], getName)
 		id.get_node("%TextEditableLabel").hint_tooltip = getName + ' in pool'
-		id.get_node("%TextEditableLabel").text = str(defaultAvailability)
 		id.get_node("%TextEditableLabel").editable = true
 		id.get_node("%TextEditableLabel").mouse_filter = Control.MOUSE_FILTER_PASS
-		
-		if defaultAvailability > 0:
+		if currentStates.has(functionVariable):
+			id.get_node("%TextEditableLabel").text = currentStates[functionVariable][0]
+			id.set_availability_state(currentStates[functionVariable][1])
+		elif defaultAvailability > 0:
+			id.get_node("%TextEditableLabel").text = str(defaultAvailability)
 			id.set_availability_state(id.ENABLED)
 		else:
+			id.get_node("%TextEditableLabel").text = str(defaultAvailability)
 			id.set_availability_state(id.DISABLED)
-		
-		#id._on_EditableLabel_text_changed(str(defaultAvailability)) # To initialize the darkening
-		if listCreature.find(i) < 16:
+		if i[3] == true:
 			oCreaturePool.add_child(id)
 		else:
 			oHeroPool.add_child(id)
 
 func initialize_traps_available(): # oTrapsAvailable
-	for i in listTrap:
-		var subtype = i[0]
-		var functionVariable = i[1]
-		var defaultAvailability = i[2]
-		var id = scnAvailableButton.instance()
-		var getName = Things.fetch_name(Things.TYPE.TRAP, subtype)
-		id.hint_tooltip = getName + ' availability'
-		id.get_node("%IconTextureRect").texture = Things.fetch_sprite(Things.TYPE.TRAP, subtype)
-		id.set_meta("variable", functionVariable)
-		id.get_node("%TextEditableLabel").editable = false
-		
-		if defaultAvailability == 1:
-			id.set_availability_state(id.ENABLED)
-		else:
-			id.set_availability_state(id.DISABLED)
-		
-		oTrapsAvailable.add_child(id)
+	var currentStates = get_existing_states(oTrapsAvailable)
+	clear_available_buttons(oTrapsAvailable)
+	for subtype in get_trapdoor_subtypes(Things.TYPE.TRAP):
+		add_trapdoor_button(oTrapsAvailable, Things.TYPE.TRAP, subtype, currentStates)
 
 func initialize_magic_available(): # oMagicAvailable
 	for i in listMagic:
@@ -314,23 +300,171 @@ func initialize_magic_available(): # oMagicAvailable
 		oMagicAvailable.add_child(id)
 
 func initialize_doors_available(): # oDoorsAvailable
-	for i in listDoor:
-		var subtype = i[0]
-		var functionVariable = i[1]
-		var defaultAvailability = i[2]
-		var id = scnAvailableButton.instance()
-		var getName = Things.fetch_name(Things.TYPE.DOOR, subtype)
-		id.hint_tooltip = getName + ' availability'
-		id.get_node("%IconTextureRect").texture = Things.fetch_sprite(Things.TYPE.DOOR, subtype)
-		id.set_meta("variable", functionVariable)
-		id.get_node("%TextEditableLabel").editable = false
-		
-		if defaultAvailability == 1:
-			id.set_availability_state(id.ENABLED)
-		else:
-			id.set_availability_state(id.DISABLED)
-		
-		oDoorsAvailable.add_child(id)
+	var currentStates = get_existing_states(oDoorsAvailable)
+	clear_available_buttons(oDoorsAvailable)
+	for subtype in get_trapdoor_subtypes(Things.TYPE.DOOR):
+		add_trapdoor_button(oDoorsAvailable, Things.TYPE.DOOR, subtype, currentStates)
+
+
+func get_room_list():
+	var roomList = listRoom.duplicate(true)
+	if oCurrentFormat.selected == Constants.ClassicFormat:
+		return roomList
+	if oConfigFileManager.current_data.has("terrain.cfg") == false:
+		return roomList
+	var roomData = oConfigFileManager.current_data["terrain.cfg"]
+	var roomIds = roomData.keys()
+	roomIds.sort()
+	for roomID in roomIds:
+		var room = roomData[roomID]
+		var roomName = room.get("Name", "")
+		if roomName == "" or roomID in [0, 1, 7] or has_room_function(roomList, roomName):
+			continue
+		var slabID = find_slab_id(room.get("SlabAssign", ""))
+		if slabID != null:
+			roomList.append([slabID, roomName, 0])
+	return roomList
+
+
+func get_creature_list():
+	var creatureList = []
+	for i in listCreature:
+		creatureList.append([i[0], i[1], i[2], listCreature.find(i) < 16, false])
+	if oCurrentFormat.selected == Constants.ClassicFormat:
+		return creatureList
+	var allSubtypes = Things.DATA_CREATURE.keys()
+	allSubtypes.sort()
+	for subtype in allSubtypes:
+		if can_add_creature(creatureList, subtype):
+			creatureList.append([subtype, Things.fetch_id_string(Things.TYPE.CREATURE, subtype), 0, true, true])
+	return creatureList
+
+
+func has_room_function(roomList, functionVariable):
+	for room in roomList:
+		if room[1] == functionVariable:
+			return true
+	return false
+
+
+func has_creature_subtype(creatureList, subtype):
+	for creature in creatureList:
+		if creature[0] == subtype:
+			return true
+	return false
+
+
+func can_add_creature(creatureList, subtype):
+	if subtype == 0 or HIDDEN_CREATURES.has(subtype):
+		return false
+	return has_creature_subtype(creatureList, subtype) == false
+
+
+func find_slab_id(slabName):
+	for slabID in Slabs.data:
+		if Slabs.data[slabID][Slabs.NAME] == slabName:
+			return slabID
+	return null
+
+
+func get_trapdoor_subtypes(thingType):
+	var subtypeList = []
+	if thingType == Things.TYPE.TRAP:
+		subtypeList = CLASSIC_TRAP_ORDER.duplicate()
+	else:
+		subtypeList = CLASSIC_DOOR_ORDER.duplicate()
+	if oCurrentFormat.selected == Constants.ClassicFormat:
+		return subtypeList
+	var allSubtypes = Things.data_structure(thingType).keys()
+	allSubtypes.sort()
+	for subtype in allSubtypes:
+		if subtype != 0 and subtypeList.has(subtype) == false:
+			subtypeList.append(subtype)
+	return subtypeList
+
+
+func add_trapdoor_button(parent, thingType, subtype, currentStates):
+	var functionVariable = Things.fetch_id_string(thingType, subtype)
+	var id = scnAvailableButton.instance()
+	var getName = Things.fetch_name(thingType, subtype)
+	id.hint_tooltip = getName + ' availability'
+	set_button_texture(id, Things.fetch_sprite(thingType, subtype), getName)
+	id.set_meta("variable", functionVariable)
+	id.get_node("%TextEditableLabel").editable = false
+	if currentStates.has(functionVariable):
+		id.set_availability_state(currentStates[functionVariable])
+	else:
+		id.set_availability_state(id.ENABLED)
+	parent.add_child(id)
+
+
+func set_creature_texture(id, subtype, isCustomCreature, getName):
+	var texture = Things.fetch_sprite(Things.TYPE.CREATURE, subtype)
+	if isCustomCreature == true:
+		texture = crop_transparent_texture(texture)
+		id.get_node("%IconTextureRect").stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	set_button_texture(id, texture, getName)
+
+
+func crop_transparent_texture(texture):
+	if texture == null:
+		return null
+	var image = texture.get_data()
+	if image == null:
+		return texture
+	image.convert(Image.FORMAT_RGBA8)
+	var cropRect = image.get_used_rect()
+	if cropRect.size == Vector2(0, 0):
+		return texture
+	var padding = max(1, int(max(cropRect.size.x, cropRect.size.y) * 0.20))
+	var width = int(cropRect.size.x) + padding * 2
+	var height = int(cropRect.size.y) + padding * 2
+	var croppedImage = Image.new()
+	croppedImage.create(width, height, false, Image.FORMAT_RGBA8)
+	croppedImage.fill(Color(0, 0, 0, 0))
+	croppedImage.blit_rect(image, cropRect, Vector2(padding, padding))
+	var croppedTexture = ImageTexture.new()
+	croppedTexture.create_from_image(croppedImage, 0)
+	return croppedTexture
+
+
+func set_button_texture(id, texture, getName):
+	id.get_node("%IconTextureRect").texture = texture
+	if texture == null:
+		add_icon_text(id, getName)
+
+
+func add_icon_text(id, getName):
+	var iconText = Label.new()
+	iconText.text = getName
+	iconText.align = Label.ALIGN_CENTER
+	iconText.valign = Label.VALIGN_CENTER
+	iconText.autowrap = true
+	iconText.anchor_right = 1.0
+	iconText.anchor_bottom = 1.0
+	iconText.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	id.get_node("%IconTextureRect").add_child(iconText)
+
+
+func get_existing_states(parent):
+	var currentStates = {}
+	for id in parent.get_children():
+		currentStates[id.get_meta("variable")] = id.availabilityState
+	return currentStates
+
+
+func get_existing_creature_data():
+	var currentStates = {}
+	for parent in [oCreaturePool, oHeroPool]:
+		for id in parent.get_children():
+			currentStates[id.get_meta("variable")] = [str(id.get_integer()), id.availabilityState]
+	return currentStates
+
+
+func clear_available_buttons(parent):
+	for id in parent.get_children():
+		parent.remove_child(id)
+		id.queue_free()
 
 
 func _on_PortalRateField_text_changed(new_text):
@@ -536,4 +670,3 @@ func _on_AdjustResearchCheckBox_toggled(button_pressed):
 	yield(get_tree(),'idle_frame')
 	if oResearchOrderCategory.visible == true:
 		oGeneratorContainer.scroll_vertical += 200
-
