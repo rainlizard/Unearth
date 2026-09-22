ea5bed8fedcc6a4887e6347703b1b73ad024b6b6
rainlizard <15337628+rainlizard@users.noreply.github.com>
2026-09-23 07:51:49 +1000

    ---

--- Scenes/OpenMap.gd
+++ Scenes/OpenMap.gd
@@ -80,8 +80,8 @@ func start():
 		if OS.has_feature("standalone") == false:
 			#open_map("C:/Games/Dungeon Keeper GOG/levels/MAP00001.SLB")
 			#open_map("C:/Games/Dungeon Keeper/levels/deepdngn/map00084.slb")
-			#open_map("C:/Games/Dungeon Keeper/levels/personal/map00001.slb")
-			open_map("C:/Games/Dungeon Keeper/campgns/dk2/map00200.slb")
+			open_map("C:/Games/Dungeon Keeper/levels/personal/map00001.slb")
+			#open_map("C:/Games/Dungeon Keeper/campgns/dk2/map00200.slb")
 			#open_map("C:/Games/Dungeon Keeper/levels/blazeend/map00001.slb")
 			#open_map("C:/Games/Dungeon Keeper/levels/traphouse/map15416.slb")
 		else:
